63fef09de14f83d3783003491f3bc6470919aacd
Sonu Ansari <786sonuansari123@gmail.com>
2026-01-18 21:53:46 +0530

    Insert operation sucessful......

--- Advance_Java_Kisan_Sir/src/com/pack1/Connection_Practice.java
+++ Advance_Java_Kisan_Sir/src/com/pack1/Connection_Practice.java
@@ -1,6 +1,8 @@
 package com.pack1;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.Connection;
 
 public class Connection_Practice {
@@ -8,17 +10,49 @@ public class Connection_Practice {
 	String user = "c##sonu";
 	String pwd = "sonu";
 
-	public void connect() {
+	public Connection connect() {
+		Connection conn = null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection conn = DriverManager.getConnection(url, user, pwd);
+			conn = DriverManager.getConnection(url, user, pwd);
 			System.out.println("Connected succefully.........");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return conn;
+	}	
+
+	public void insertData() {
+		try {
+			int id=Integer.parseInt(IO.readln("Enter Employee ID:"));
+			String fname=IO.readln("Enter Employee first name");
+			String lname=IO.readln("Enter Employee last name: ");
+			double sal=Double.parseDouble(IO.readln("Enter Salary"));
+			String add=IO.readln("Enter address");
+			Connection conn=connect();
+			Statement stm=conn.createStatement();
+			int row=stm.executeUpdate("insert into Employee values ("+id+",'"+fname+"','"+lname+"',"+sal+", '"+add+"')");
+			if(row>0) {
+				IO.print("Data Inserted Sucessfully.......");
+			}else {
+				System.err.println("Data not inserted....");
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public static void main(String[] args) {
-		new Connection_Practice().connect();
+		Connection_Practice c1=new Connection_Practice();
+		IO.println("1) Insert Data");
+		int choice=Integer.parseInt(IO.readln("Enter your choice"));
+		switch(choice) {
+		case 1->{
+			c1.insertData();
+		}
+			default ->System.err.println("Invalid choice.....");
+		};
+		
 	}
 }
