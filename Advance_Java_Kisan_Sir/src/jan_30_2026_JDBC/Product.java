138875f3b86b63bfb09050a88215cd6d684e5681
Sonu Ansari <786sonuansari123@gmail.com>
2026-02-01 15:46:04 +0530

    Assignment2: Write a jdbc program as follows: Note:Create a table:-Product (U can create table from database).               Table Columns:-productId,productName,productPrice,productQty. Give Option to the user:           1.Insert productdetails into product table.           2.Retrieve productdetails in forward direction.           3.Retrieve productdetails in reverse direction.           4.Retrieve 3rd record from top.           5.Retrieve 3rd record from bottom.           6.Retrieve last three record from product table.

--- Advance_Java_Kisan_Sir/src/jan_30_2026_JDBC/Product.java
+++ Advance_Java_Kisan_Sir/src/jan_30_2026_JDBC/Product.java
@@ -3,6 +3,7 @@ package jan_30_2026_JDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Product {
 	String url = "jdbc:oracle:thin:@localhost:1521:orcl";
@@ -47,16 +48,86 @@ public class Product {
 
 	}
 //2.Retrieve productdetails in forward direction.
-	
+	public void retriceForwardDirection() {
+		Connection conn=connect();
+		try {
+			PreparedStatement prep =conn.prepareStatement("Select * from Product5");
+			ResultSet rest=prep.executeQuery();
+			System.out.println("PRODUCTID\tPRODUCTNAME\t\tPRODUCTPRICE\t\tPRODUCTQTY");
+			while(rest.next()) {
+				System.out.println(rest.getInt(1)+"\t\t"+rest.getString(2)+"\t\t\t"+rest.getDouble(3)+"\t\t\t"+rest.getInt(4));
+			}
+			System.out.println();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+//3.Retrieve productdetails in reverse direction.
+	public void retriveReverseDirection() {
+		Connection conn=connect();
+		try {
+			PreparedStatement prep =conn.prepareStatement("Select * from Product5",ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			ResultSet rest=prep.executeQuery();
+			System.out.println("PRODUCTID\tPRODUCTNAME\t\tPRODUCTPRICE\t\tPRODUCTQTY");
+			rest.afterLast();
+			while(rest.previous()) {
+				System.out.println(rest.getInt(1)+"\t\t"+rest.getString(2)+"\t\t\t"+rest.getDouble(3)+"\t\t\t"+rest.getInt(4));
+			}
+			System.out.println();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+//4.Retrieve 3rd record from top.
+	public void retriveRowWise(int row) {
+		Connection conn=connect();
+		try {
+			PreparedStatement prep =conn.prepareStatement("Select * from Product5",ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			ResultSet rest=prep.executeQuery();
+			System.out.println("PRODUCTID\tPRODUCTNAME\t\tPRODUCTPRICE\t\tPRODUCTQTY");
+			while(rest.absolute(row)) {
+				System.out.println(rest.getInt(1)+"\t\t"+rest.getString(2)+"\t\t\t"+rest.getDouble(3)+"\t\t\t"+rest.getInt(4));
+				break;
+			}
+			System.out.println();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 	public static void main(String[] args) {
-
-		IO.println("1.Insert productdetails into product table.\n" + "2.Retrieve productdetails in forward direction.\n"
-				+ "3.Retrieve productdetails in reverse direction.\n" + "4.Retrieve 3rd record from top.\n"
-				+ "5.Retrieve 3rd record from bottom.\n" + "6.Retrieve last three record from product table.");
-		int choice = Integer.parseInt(IO.readln("Enter your choice...."));
 		Product p = new Product();
-		p.InsertProductdetails();
+
+		while(true) {
+			IO.println("1.Insert productdetails into product table.\n" + 
+			"2.Retrieve productdetails in forward direction.\n"+ 
+			"3.Retrieve productdetails in reverse direction.\n" + 
+			"4.Retrieve 3rd record from top.\n"+ 
+			"5.Retrieve 3rd record from bottom.\n" + 
+			"6.Retrieve last three record from product table.");
+			int choice = Integer.parseInt(IO.readln("Enter your choice...."));
+			switch(choice) {
+			case 1->{
+				p.InsertProductdetails();	
+			}
+			case 2->{
+				p. retriceForwardDirection();
+			}
+			case 3->{
+				p.retriveReverseDirection();
+			}
+			case 4->{
+				int rowNo = Integer.parseInt(IO.readln("Enter row Number"));
+				p.retriveRowWise(rowNo);
+			}
+			default -> System.err.println("Invalid choice....");
+			}
+		}
+		
 
 	}
 }
