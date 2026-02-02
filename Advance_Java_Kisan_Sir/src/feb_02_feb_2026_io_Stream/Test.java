b02e573e1ea4fe1e9a72a2bc94b59874a0a499f1
Sonu Ansari <786sonuansari123@gmail.com>
2026-02-02 12:50:24 +0530

    IO stream in JDBC retrive image in database and insert image into database to file

---
+++ Advance_Java_Kisan_Sir/src/feb_02_feb_2026_io_Stream/Test.java
@@ -0,0 +1,73 @@
+package feb_02_feb_2026_io_Stream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Test {
+	String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+	String user = "c##sonu";
+	String pwd = "sonu";
+
+//connection 
+	public Connection connect() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, pwd);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public void insertImage() {
+		Connection conn = connect();
+		try {
+			PreparedStatement prep1 = conn.prepareCall("insert into mydata values (?,?)");
+			prep1.setString(1, "100");
+			FileInputStream fis = new FileInputStream("F:\\img.png");
+			prep1.setBlob(2, fis, fis.available());
+			int rowCount = prep1.executeUpdate();
+			if (rowCount > 0) {
+				System.out.println("Image Inserted Sucessfully.......");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void retriveImage() {
+		Connection conn = connect();
+		try {
+			PreparedStatement prep2 = conn.prepareCall("select PIC_DATA  from mydata where id=?");
+			prep2.setString(1, "100");
+			ResultSet rs = prep2.executeQuery();
+			if (rs.next()) {
+				Blob b = rs.getBlob(1);
+				byte arr[] = b.getBytes(1, (int) b.length());
+				FileOutputStream fos = new FileOutputStream("F:\\\\img2.png");
+				fos.write(arr);
+				System.out.println("Image Retrived........");
+
+			}else {
+				System.err.println("image not found");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Test t1 = new Test();
+		//t1.insertImage();
+		t1.retriveImage();
+	}
+}
---
+++ CoreJava/src/stream_API_Logical_Program/Assignment.java
@@ -0,0 +1,35 @@
+	package stream_API_Logical_Program;
+	
+	/*
+	
+	
+	
+	
+	
+	
+	
+	WAP to sort a given integer array in ascending order using Stream API.
+	
+	WAP to sort a given String array in descending order using Stream API.
+	
+	WAP to remove duplicate elements from an integer array using Stream API.
+	
+	WAP to find the first element of a Stream from a given array.
+	
+	WAP to find the maximum number from an integer array using Stream API.
+	
+	WAP to find the minimum number from an integer array using Stream API.
+	
+	WAP to check whether any number in the array is divisible by 5 using Stream API.
+	
+	WAP to filter all names starting with the letter ‘S’ from a String array using Stream API.
+	
+	WAP to calculate the sum of all elements of an integer array using Stream API.
+	
+	WAP to convert a Stream into a List from a given array.
+	
+	WAP to filter all numbers between 10 and 100 from an integer array using Stream API.
+	
+	WAP to check whether all elements in an integer array are positive using Stream API.
+	
+	WAP to skip the first 3 elements from an integer array using Stream API and print the remaining elements.*/
